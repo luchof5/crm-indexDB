27c4d2038bdc59c979049185621d4393f9e07de8
Luis Furtado <lucho_l.f@hotmail.com>
2023-08-30 13:40:39 -0300

    cleinte agregado

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -35,7 +35,44 @@
 
             return;
         }
+
+        // Crea un objeto con la información
+
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa
+        }
+
+        cliente.id = Date.now();
+
+        crearNuevoCliente(cliente);
     }
+
+
+    function crearNuevoCliente(cliente) {
+        const transaction = DB.transaction(['crm'], 'readwrite');
+
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.add(cliente);
+
+        transaction.onerror = function() {
+            imprimirAlerta('Hubo un error', 'error');
+        }
+
+        transaction.oncomplete = function() {
+            console.log('CLeinte agregado');
+
+            imprimirAlerta('El cliente agregó correctamente');
+
+            setTimeout(() => {
+                window.location.href = 'index.html'
+            }, 3000);
+        }
+    }
+
 
     function imprimirAlerta(mensaje, tipo) {
 
@@ -44,7 +81,7 @@
         if(!alerta){
             // Crear alerta
             const divMensaje = document.createElement('div');
-            divMensaje.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+            divMensaje.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center', 'border', 'alerta');
 
             if(tipo === 'error') {
                 divMensaje.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
